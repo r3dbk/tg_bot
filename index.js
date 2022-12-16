ae7dd770ab01828ae4aeff02d8a32ed9c0430442
Sviatoslav <113039372+r3dbk@users.noreply.github.com>
2022-12-16 16:29:17 +0300

    1.1

--- index.js
+++ index.js
@@ -11,24 +11,23 @@ bot.onText(/\/start/, (msg) => {
       keyboard: [["Отправь мне картинку", "Отправь мне аудио"], ["/start"]],
     },
   });
-  bot.on('message', (msg) => {
+  bot.on("message", (msg) => {
     var location = "location";
     if (msg.text.indexOf(location) === 0) {
-        bot.sendLocation(msg.chat.id,44.97108, -104.27719);
-        bot.sendMessage(msg.chat.id, "Наши координаты");
+      bot.sendLocation(msg.chat.id, 44.97108, -104.27719);
+      bot.sendMessage(msg.chat.id, "Наши координаты");
     }
-});
-  bot.on("message", (msg) => {
-    if (msg.text.toString() === "Отправь мне картинку") {
-      bot.sendPhoto(
-        msg.chat.id,
-        "https://static.tildacdn.com/tild6364-3230-4537-b661-323034363738/_-14_1.png",
-        { caption: "Айтигенио приветствует тебя" }
-      );
-    }
-    if (msg.text.toString() === "Отправь мне аудио") {
-      bot.sendAudio(msg.chat.id, "noname.wma");
-    }
+    bot.on("message", (msg) => {
+      if (msg.text.toString() === "Отправь мне картинку") {
+        bot.sendPhoto(
+          msg.chat.id,
+          "https://static.tildacdn.com/tild6364-3230-4537-b661-323034363738/_-14_1.png",
+          { caption: "Айтигенио приветствует тебя" }
+        );
+      }
+      if (msg.text.toString() === "Отправь мне аудио") {
+        bot.sendAudio(msg.chat.id, "noname.wma");
+      }
+    });
   });
-  
 });
